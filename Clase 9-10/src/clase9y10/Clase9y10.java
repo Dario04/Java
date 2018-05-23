9187aa10945a4f91c89291c746f23c29434a18a2
DonJoseManuelDelVillamonte <39318015+DonJoseManuelDelVillamonte@users.noreply.github.com>
2018-05-22 22:01:47 -0300

    Add files via upload

---
+++ Clase 9-10/src/clase9y10/Clase9y10.java
@@ -0,0 +1,40 @@
+package clase9y10;
+
+public class Clase9y10 {
+//static int dim=10;
+static void main(String[] arguments) {
+		// TODO Auto-generated method stub
+        // segun el profe, esta es la clase 10
+		// pero es la clase 11
+//		int var=9;
+//		for (var=0;var<20;var++) {
+//		System.out.println(var);
+//	}
+//	System.out.println("=============");
+//	System.out.println(var);
+//		int dim2=99;
+//		System.out.println(dim2);
+//		int[] vec =  new int[dim];
+//
+//		cargarVector(vec);
+		
+//		System.out.println("==============");
+//		for (int i = 0; i < dim; i++){
+//			System.out.println(vec[i] + " ");
+		}
+//		int variable=99;
+//		System.out.println(cargarVector(variable));
+//		System.out.println(variable);
+//		
+	
+	//}//aca termina el metodo main
+//public static void cargarVector(int[] vec){
+//    for (int i = 0; i < dim; i++){
+//    	vec[i]=(int)(Math.random())*100;
+    //}
+//	variable=80;
+//	char var = 'a';
+//	return var;
+	}
+//}
+//aca termina la clase
---
+++ Clase 9-10/src/clase9y10/Clase9y10v2.java
@@ -0,0 +1,32 @@
+package clase9y10;
+
+public class Clase9y10v2 {
+
+	public static void main(String[] arguments) {
+		// TODO Auto-generated method stub
+//        String[] names = { "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt",
+//        		"Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda",
+//        		"Aaron", "Kate"};
+		String[] names = new String[10];
+        int[] times = new int[names.length];
+        cargoCorredor(names);
+        CargoTiempo(times);
+        int masrapido = calculoElMasRapido(times);
+        for (int i = 0; i < names.length; i++) {
+        	System.out.println(names[i] + ": " + times[i]);
+        }
+	}
+	static void cargoCorredor(String[] names) {
+	if 	
+	}
+	static void CargoTiempo(int[] times) {
+	
+	}
+	static int calculoElMasRapido(int[] times) {
+	
+	int mayor;
+	
+	return mayor;
+	}
+
+}
