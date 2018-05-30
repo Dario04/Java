b9dcef376a9dcf898ab8ee7697bde139b8d86cff
DonJoseManuelDelVillamonte <39318015+DonJoseManuelDelVillamonte@users.noreply.github.com>
2018-05-29 21:59:27 -0300

    Add files via upload

--- Clase 9-10/src/clase9y10/Clase9y10v2.java
+++ Clase 9-10/src/clase9y10/Clase9y10v2.java
@@ -1,32 +1,59 @@
 package clase9y10;
 
+import java.util.Scanner;
+
 public class Clase9y10v2 {
+	// tienen que completar los métodos e imprimir los resultados en el main
 
 	public static void main(String[] arguments) {
-		// TODO Auto-generated method stub
-//        String[] names = { "Elena", "Thomas", "Hamilton", "Suzie", "Phil", "Matt",
-//        		"Alex", "Emma", "John", "James", "Jane", "Emily", "Daniel", "Neda",
-//        		"Aaron", "Kate"};
-		String[] names = new String[10];
-        int[] times = new int[names.length];
-        cargoCorredor(names);
-        CargoTiempo(times);
-        int masrapido = calculoElMasRapido(times);
-        for (int i = 0; i < names.length; i++) {
-        	System.out.println(names[i] + ": " + times[i]);
-        }
-	}
-	static void cargoCorredor(String[] names) {
-	if 	
-	}
-	static void CargoTiempo(int[] times) {
-	
-	}
-	static int calculoElMasRapido(int[] times) {
-	
-	int mayor;
-	
-	return mayor;
+		String[] names = new String[4];
+
+		int[] times = new int[names.length];
+
+		cargoCorredores(names);
+
+		cargoTiempos(times);
+
+		for (int i = 0; i < names.length; i++) {
+			System.out.println(names[i] + ": " + times[i]);
+		}
+
+		calculoElMasRapido(times, names);
+
 	}
 
+	static void cargoCorredores(String[] names) {
+		Scanner sc = new Scanner(System.in);
+
+		for (int i = 0; i < names.length; i++) {
+			System.out.println("Por favor ingrese el nombre del corredor " + (i + 1));
+			names[i] = sc.nextLine();
+		}
+	}
+
+	static void cargoTiempos(int[] times) {
+		// cosas con Math.random();
+		for (int i = 0; i < times.length; i++) {
+			times[i] = (int) (Math.random() * 100);
+		}
+	}
+
+	static void calculoElMasRapido(int[] times, String[] names) {
+		// mas cosas
+		int menor = times[0];
+		int pos = 0;
+		int aux = 0;
+
+		for (int i = 0; i < times.length; i++) {
+			if (menor >= times[i]) {
+				menor = times[i];
+				aux = pos;
+				pos = i;
+			} // estructura para calcular el más rapido
+		}
+
+		// imprimo el mas rapido
+		System.out.println("El corredor " + names[pos] + " mas rapido con un tiempo de: " + times[pos]);
+		System.out.println("El segundo corredor mas rapido fue " + names[aux] + " con un tiempo de: " + times[aux]);
+	}
 }
